140e54e07a93c85dbafabf67064f01068bdfc85a
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-01-19 12:22:06 -0500

    adding MockV3Aggretor from chainlink

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,10 +8,12 @@ contract FundMe {
   using SafeMathChainlink for uint256;
 
   mapping(address => uint256) public cheese;
-  address public owner; 
+  address public owner;
+  AggregatorV3Interface public priceFeed; 
 
-  constructor() public {
-   owner = msg.sender;
+  constructor(address _priceFeed) public {
+    priceFeed = AggregatorV3Interface(_priceFeed);
+    owner = msg.sender;
   } 
 
 
@@ -22,12 +24,10 @@ contract FundMe {
   }
 
   function getVerison() public view returns (uint256) {
-    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
     return priceFeed.version();
   }
 
   function getPrice() public view returns (uint256) {
-    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
      (,int256 answer,,,) = priceFeed.latestRoundData();
          // ETH/USD rate in 18 digit 
       return uint256(answer * 10000000000);
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,7 +3,17 @@ from scripts.helpful_scripts import get_account
 
 def deploy_simple_storage():
   account = get_account()
-  fund_me = FundMe.deploy({"from": account})
+
+  if network.show_active() != "development":
+    price_feed_address = config["networks"][network.show_active()][
+      "eth_usd_price_feed"
+    ]
+
+  fund_me = FundMe.deploy(
+    price_feed_address,
+    {"from": account}, publish_source=True)
+
+ 
   print(f"Contract deployed to {fund_me.address}")
 
 def main(): 
