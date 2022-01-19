61ef95210272760fcb62f75990c31bc3e5893367
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-01-19 18:28:47 -0500

    test for fundMe

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,15 +1,21 @@
 from brownie import network, config, accounts, MockV3Aggregator 
 from web3 import Web3
 
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     else:
       return accounts.load("stu-account")
 
 def deploy_mocks():
-   print(f"The active netowrk is {network.show_active()}")
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
-      MockV3Aggregator.deploy(18, Web3.toWei(2000, "ether"), {"from": get_accout()})
-    #use the most recently deployed MockV3Aggregator
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
+    print("Mocks Deployed!")
